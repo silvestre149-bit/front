95cca860a00b5ead553b8498b75ee6cc4a159317
silvestre149-bit <74218849+silvestre149-bit@users.noreply.github.com>
2022-08-02 17:42:17 -0300

    Delete src/api directory

--- src/api/cadastrar/index.js
+++
@@ -1,7 +0,0 @@
-import { get } from 'mongoose';
-import { post } from '../request';
-
-export async function cadastroTCC() {
-    const requisicao = await post('/projeto').then(requisicao => requisicao.data)
-    return requisicao;
-}
--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-
-import { logarUsuario } from './auth';
-import { del, post, put, get } from './request';
-
-export { logarUsuario, del, post, put, get };
-
-
-
